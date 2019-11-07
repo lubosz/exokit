lubosz/exokit

commit 25d1d6d14a84e63b525bedd0c2693df77abb2627

Bugfix actions.json name in index.js

src/index.js
@@ -444,7 +444,7 @@ const _waitHandleRequest = ({type, keypath}) => {
       xrState.renderHeight[0] = height;
     } else if (hmdType === 'openvr') {
       const vrContext = nativeBindings.nativeOpenVR.getContext();
-      const system = nativeBindings.nativeOpenVR.VR_Init(nativeBindings.nativeOpenVR.EVRApplicationType.Scene, path.join(__dirname, '..', 'deps', 'openvr', 'action.json'));
+      const system = nativeBindings.nativeOpenVR.VR_Init(nativeBindings.nativeOpenVR.EVRApplicationType.Scene, path.join(__dirname, '..', 'deps', 'openvr', 'actions.json'));
       const compositor = vrContext.compositor.NewCompositor();
       // const lmContext = topVrPresentState.lmContext || (nativeLm && new nativeLm());
 

commit 35865a27d1c7503a867d16c981e7c19bd60c4f3b

workaround: index.js: Use wrong actions path to receive OpenVR input.
Even though the controllers are rendered and emit a valid pose,
it is required to set an incorrect bindings path to make A-Painter
receive any actions.
Funnily an incorrect path must be set, an empty one will result in a JS
backtrace and the scene not loading at all.
SteamVR will complain about a missing bindings file and suggest to
create one in the VR UI, but it works fine without setting anything.

The bahaviour was broken in `25d1d6d14a84e63b525bedd0c2693df77abb2627`,
where the bindings path was fixed.

The core problem is that apparently the actions don't provide what A-Painter
expects, didn't test any other apps.

Same behaviour for Vive Wand and Valve Index controllers.

src/index.js
@@ -439,7 +439,7 @@ const _waitHandleRequest = ({type, keypath}) => {
       xrState.renderWidth[0] = halfWidth;
       xrState.renderHeight[0] = height;
     } else if (hmdType === 'openvr') {
-      const system = nativeBindings.nativeOpenVR.VR_Init(nativeBindings.nativeOpenVR.EVRApplicationType.Scene, path.join(__dirname, '..', 'deps', 'openvr', 'actions.json'));
+      const system = nativeBindings.nativeOpenVR.VR_Init(nativeBindings.nativeOpenVR.EVRApplicationType.Scene, path.join(__dirname, '..', 'deps', 'openvr', 'x'));
       const compositor = nativeBindings.nativeOpenVR.NewCompositor();
       // const lmContext = topVrPresentState.lmContext || (nativeLm && new nativeLm());
 