lubosz/exokit

commit a26eb29df116b3d082d4080c7989686004747c8c

Add Valve index controller detection in VR.js

src/VR.js
@@ -708,6 +708,8 @@ const controllerIDs = {
   fake: 'OpenVR Gamepad',
   openvr: 'OpenVR Gamepad',
   openvrTracker: 'OpenVR Tracker',
+  indexLeft: 'Valve Index (Left)',
+  indexRight: 'Valve Index (Right)',
   oculusLeft: 'Oculus Touch (Left)',
   oculusRight: 'Oculus Touch (Right)',
   // oculusMobile: 'Oculus Go',
@@ -726,8 +728,9 @@ function getGamepads() {
     let hmdType = getHMDType();
     if (hmdType === 'openvr') {
       const vrSystem = nativeOpenVR.GetGlobalSystem();
-      const model = vrSystem.GetModelName();
-      console.log('got model name'); // XXX
+      if (/^Knuckles/.test(vrSystem.GetModelName(0) || vrSystem.GetModelName(1))) {
+        hmdType = 'index';
+      }
     }
 
     if (!globalGamepads) {

commit d4ec938a4cb0979e67f95ab9d7a6c013a550f680

workaround: VR.js: Don't set HMD type to 'index' to fix Knuckles.
Ironically this makes controllers render again in A-Painter when using
the Valve Index / Knuckles controllers. This was broken in
`a26eb29df116b3d082d4080c7989686004747c8c` which supposedly adds support
for them.

I suspect that either OpenVR compatibility does it's job here and works better
than exokit's internal handling or A-Painter only displays known controllers.

src/VR.js
@@ -750,7 +750,7 @@ function getGamepads() {
     if (hmdType === 'openvr') {
       const vrSystem = nativeOpenVR.GetGlobalSystem();
       if (vrSystem && /^Knuckles/.test(vrSystem.GetModelName(0) || vrSystem.GetModelName(1))) {
-        hmdType = 'index';
+        // hmdType = 'index';
       }
     }
 